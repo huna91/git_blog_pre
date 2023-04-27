3434ba90315eae8bb74a3cbb104d1811c8020ecd
unknown <gmyhy0510@naver.com>
2023-04-27 11:54:56 +0900

    회전됨

--- portfolio/src/components/BasicBall.jsx
+++ portfolio/src/components/BasicBall.jsx
@@ -1,71 +1,80 @@
-import React, { useRef, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { Sphere, MeshDistortMaterial } from "@react-three/drei";
 import { useFrame } from "@react-three/fiber";
 
-const BasicBall = ({ color, position }) => {
+const BasicBall = ({ wheel, setWheel }) => {
   const ball = useRef();
-  let angle = 0;
-  let w = window.innerWidth;
-  let h = window.innerHeight;
-  const Base_W = w / 0.3;
-  const Base_H = h / 0.2;
+  let position = useRef();
+  const figure = 8;
+  const Angle = 360 / figure;
   const radian = (angle) => (angle * Math.PI) / 180;
-  const radius = 15;
+  const radius = 10;
+
+  const [move, setMove] = useState(0);
+  const [mcon, setMcon] = useState(false);
 
   // 빨,주,노,초,파,남,보 7개
   const [ballColor, setBallColor] = useState([
-    { color: "#ff0000", x: 11, y: 10 },
-    { color: "#ff8c00", x: 8.5, y: 17.5 },
-    { color: "#ffff00", x: 3.5, y: 20 },
-    { color: "#008000", x: -1.5, y: 17.5 },
-    { color: "#0000ff", x: -4, y: 12.5 },
-    { color: "#4b0082", x: -1.5, y: 7.5 },
-    { color: "#800080", x: 3.5, y: 5 },
-    {
-      color:
-        "linear-gradient(to right,#ee5252,#e79d3c 12.5%,#eaf510 25%,#53fc05 37.5%, #05fcc2 50%, #2343f8 62.5%, #f41ff4 75%,  #fc2626)",
-    },
+    { color: "#ff0000" },
+    { color: "#ff8c00" },
+    { color: "#ffff00" },
+    { color: "#008000" },
+    { color: "#0000ff" },
+    { color: "#4b0082" },
+    { color: "#800080" },
+    { color: "#ffffff" },
   ]);
+  // console.log(position.current.position.x);
   useFrame((state, delta) => {
-    // ball.current.rotation.y += 1 * delta;
+    if (wheel === 100) {
+      setMcon(true);
+      ball.current.rotation.y += radian(1);
+    } else if (wheel === -100) {
+      setMcon(false);
+      ball.current.rotation.y -= radian(1);
+    }
+
+    if (
+      Math.floor(Math.abs(ball.current.rotation.y) * 1000) %
+        Math.floor(radian(45) * 1000) <
+        10 &&
+      wheel !== 0
+    ) {
+      console.log(mcon);
+      setWheel(0);
+      mcon ? setMove((move + 1) % 8) : setMove((8 - move - 1) % 8);
+    }
   });
   return (
     <group>
-      <mesh scale={(2, 2, 2)} ref={ball}>
+      <mesh scale={(2, 2, 2)} ref={ball} position={[-30, 0, -5]}>
         {ballColor.map((val, ind) => {
-          return ind === 0 ? (
+          return (
             <Sphere
               visible
               args={[1.2, 200, 400]}
               position={[
-                Math.cos(radian( (360 / 8) * ind)) * radius,
+                Math.cos(radian(Angle * ind)) * radius,
                 0,
-                Math.cos(radian(angle + (360 / 8) * ind)) * radius,
+                Math.sin(radian(Angle * ind)) * radius,
               ]}
+              ref={position}
             >
-              <MeshDistortMaterial
-                color={val.color}
-                attach="material"
-                distort={0.6}
-                speed={2}
-                roughness={0}
-              />
-            </Sphere>
-          ) : (
-            <Sphere
-              visible
-              args={[1.2, 50, 60]}
-              position={[
-                Math.cos(radian(angle + (360 / 8) * ind)) * radius,
-                0,
-                Math.cos(radian(angle + (360 / 8) * ind)) * radius,
-              ]}
-            >
-              <meshStandardMaterial
-                color={val.color}
-                attach="material"
-                roughness={0.2}
-              />
+              {move === ind ? (
+                <MeshDistortMaterial
+                  color={val.color}
+                  attach="material"
+                  distort={0.6}
+                  speed={2}
+                  roughness={0}
+                />
+              ) : (
+                <meshStandardMaterial
+                  color={val.color}
+                  attach="material"
+                  roughness={0.2}
+                />
+              )}
             </Sphere>
           );
         })}
--- portfolio/src/pages/Main.jsx
+++ portfolio/src/pages/Main.jsx
@@ -4,51 +4,29 @@ import { BasicBall, MovingBall } from "../components";
 import { CameraControls, OrbitControls } from "@react-three/drei";
 
 const Main = () => {
-  // 빨,주,노,초,파,남,보 7개
-  // const [ballColor, setBallColor] = useState([
-  //   { color: "#ff0000", x: 11, y: 10 },
-  //   { color: "#ff8c00", x: 8.5, y: 17.5 },
-  //   { color: "#ffff00", x: 3.5, y: 20 },
-  //   { color: "#008000", x: -1.5, y: 17.5 },
-  //   { color: "#0000ff", x: -4, y: 12.5 },
-  //   { color: "#4b0082", x: -1.5, y: 7.5 },
-  //   { color: "#800080", x: 3.5, y: 5 },
-  // ]);
-  function right_move() {
-    // const temp = ballColor[0];
-  }
+  const [wheel, setWheel] = useState(0);
 
   return (
     <div>
       <div className="canvas_wrap">
-        <div className="button">
-          <button onClick={() => right_move()}>왼</button>
-          <button>오</button>
-        </div>
+        <div className="button"></div>
         <Canvas
           // orthographic
+          onWheel={(e) => setWheel(e.deltaY)}
           camera={{
-            zoom: 15,
+            zoom: 20,
             position: [0, 800, 0],
           }}
         >
-          <OrbitControls />
+          {/* <OrbitControls /> */}
           <ambientLight intensity={0.3} />
           <pointLight intensity={0.7} position={[3, 13, 5]} />
-          {/* {ballColor.map((val, ind) => {
-            return ind == 0 ? (
-              <MovingBall
-                position={[val.x - 13, 0, val.y - 13]}
-                color={val.color}
-              />
-            ) : (
-              <BasicBall
-                position={[val.x - 13, 0, val.y - 13]}
-                color={val.color}
-              />
-            );
-          })} */}
-          <BasicBall />
+          <BasicBall
+            wheel={wheel}
+            setWheel={(e) => {
+              setWheel(e);
+            }}
+          />
         </Canvas>
       </div>
     </div>
